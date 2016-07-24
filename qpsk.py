7e0b5979aa63ac794fb66c4992e34a2d464a6d9a
Mathias Helsen <helsen.mathias@gmail.com>
2016-07-24 13:16:43 +0200

    experimenting with limiting cases

--- qpsk.py
+++ qpsk.py
@@ -1,15 +1,22 @@
 import numpy as np
 import scipy.signal 
 import matplotlib.pyplot as plt
+"""
+Currently, this is configured for a very interesting case. Should we put the
+symbolRate higher than 0.5, we will not succeed in succesfull demodulation.
+However if we take the modulated spectrum and shift it down to 0.5 f_carrier, 
+we do succeed in demodulation at half the carrier frequency.
+
+"""
 
 # Number of periods to calculate
 N = 4096
 # Oversampling ratio
 Nos = 64
 # Symbol rate relative to carrier
-symbolRate = 0.16
+symbolRate = 0.5
 # The bandwidth off the BP filter rel to symbol rate
-relBW = 0.52
+relBW = 2.0
 # Number of phases
 Nphases = 8
 
@@ -22,7 +29,7 @@ symbols = (2.0*np.pi/Nphases)*(np.random.random_integers(0, Nphases-1, symbolRat
 # The actual waveform
 x = [np.sin(2.0*np.pi*t[i] + symbols[(i/Nos)*symbolRate]) for i in range(N*Nos)]
 # Add some noise for good measure
-x = x + np.random.normal(loc=0.0, scale=1.0, size=len(x))
+x = x + np.random.normal(loc=0.0, scale=0.0001, size=len(x))
 
 # Internal constants for decimation and filtering
 LPorder = (Nos*2)
@@ -59,21 +66,22 @@ print freq[0], freq[1]-freq[0]
 
 # Calculate where the carrier is found and the admissible BW 
 indexCenter = int(round(1.0/(freq[1]-freq[0])))
-widthSideband = int(round(relBW*symbolRate/(freq[1]-freq[0])))
+widthSideband = int(round(0.5*relBW*symbolRate/(freq[1]-freq[0])))
 lo = indexCenter - widthSideband
 up = indexCenter + widthSideband
+print lo, indexCenter, up
 
 # Use only the portion of the spectrum allowed by the brickwall filter
 x_hat_prime = np.zeros(len(x_hat)) + 1j*np.ones(len(x_hat))*1.0e-6
-x_hat_prime[lo:up] = x_hat[lo:up]
+x_hat_prime[:up-lo] = x_hat[lo:up]
 power_prime = 20.0*np.log10(np.absolute(x_hat_prime))
 
 # Inverse FFT
 x_prime = np.fft.irfft(x_hat_prime)
 # IQ calculation
-I_prime = np.sin(2.0*np.pi*t)*x_prime
+I_prime = np.sin(1.0*np.pi*t)*x_prime
 I_prime = np.convolve(I_prime, np.ones((LPorder,))/LPorder, mode='same')
-Q_prime = np.cos(2.0*np.pi*t)*x_prime
+Q_prime = np.cos(1.0*np.pi*t)*x_prime
 Q_prime = np.convolve(Q_prime, np.ones((LPorder,))/LPorder, mode='same')
 
 # Save IQ 
